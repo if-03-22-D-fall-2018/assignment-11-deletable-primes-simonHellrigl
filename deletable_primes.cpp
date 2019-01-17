0b002528c4c77071f48ae75da3e6e1488e71e9b5
simonHellrigl <simsi.hellrigl@gmail.com>
2019-01-17 12:55:21 +0100

    remove tests are working

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -10,3 +10,24 @@
  * Implementation of deletable_primes.h.
  * ----------------------------------------------------------
  */
+
+ #include "deletable_primes.h"
+ #include <math.h>
+ unsigned long remove_digit(int index, unsigned long number)
+ {
+   unsigned long left;
+   unsigned long rigth;
+   unsigned long new_number;
+
+   left=number/pow(10,index+1);
+   rigth=number%(unsigned long)pow(10,index);
+   new_number=left*pow(10,index)+rigth;
+   return new_number;
+
+ }
+
+ int get_ways(unsigned long p)
+ {
+   return 0;
+
+ }
--- deletable_primes.h
+++ deletable_primes.h
@@ -13,4 +13,8 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
+
+
+unsigned long remove_digit(int index, unsigned long number);
+int get_ways(unsigned long p);
 #endif
