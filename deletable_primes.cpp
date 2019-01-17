00044da50689a44695b46d63ed78ce2409d42115
simonHellrigl <simsi.hellrigl@gmail.com>
2019-01-17 18:43:01 +0100

    save

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -12,7 +12,10 @@
  */
 
  #include "deletable_primes.h"
- 
+
+ int get_length(int num);
+ bool is_prime(int num);
+
  unsigned long remove_digit(int index, unsigned long number)
  {
    unsigned long left;
@@ -28,6 +31,55 @@
 
  int get_ways(unsigned long p)
  {
-    return 0;
+  int number = (int)p;
+  int counter = 0;
+  int temp = number;
+  int len =get_length(temp);
+  int ways_temp;
+
+  for (int i = 0; i < len; i++)
+  {
+    ways_temp = get_ways(remove_digit(i,temp));
+    if (len != 1 &&  is_prime(ways_temp))
+    {
+
+      counter += ways_temp;
+    }
+    if (get_length(number) == 1 )
+    {
+      if (is_prime(temp) == true)
+      {
+        counter ++;
+      }
+    }
+
+  }
+  return counter;
 
  }
+ int get_length(int number)
+{
+  int counter = 0;
+  int temp = number;
+  while (temp != 0)
+  {
+    temp = temp/10;
+    counter++;
+  }
+  return counter;
+}
+
+bool is_prime(int number)
+{
+  int index = 2;
+  int temp = number;
+  while (index <= sqrt(number))
+  {
+    if (temp % (int) index == 0)
+    {
+      return false;
+    }
+    index++;
+  }
+  return true;
+}
